983b0c0322ceb4b6cacb357e6c58e73bbabfe85e
amanu212 <amanalex66@gmail.com>
2025-08-02 18:09:00 +0300

    updated

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,7 @@ const Search = () => {
       const data = await fetchUserData({ username, location, minRepos });
       setUsers(data.items); // search API returns { total_count, items: [...] }
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
