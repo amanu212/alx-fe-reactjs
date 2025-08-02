8f6699a6b666d705109263432afcf28c73eeb3ab
amanu212 <amanalex66@gmail.com>
2025-08-02 17:48:06 +0300

    updated

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
+export const advancedUserSearch = async ({ username, location, minRepos }) => {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    let query = "";
+
+    if (username) query += `${username} in:login`;
+    if (location) query += ` location:${location}`;
+    if (minRepos) query += ` repos:>=${minRepos}`;
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${encodeURIComponent(query)}`
+    );
     return response.data;
   } catch (error) {
     throw error;
