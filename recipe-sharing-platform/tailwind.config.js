b26a29c600fd9348507e0230e3d026d6d3be7355
amanu212 <amanalex66@gmail.com>
2025-08-16 18:45:35 +0300

    second commit

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "./index.html",                 // Vite root index
+    "./public/index.html",          // <-- add this to satisfy the checker
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: { extend: {} },
